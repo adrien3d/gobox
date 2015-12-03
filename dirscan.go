c584b0da2b2c5c5306c1b23931bf6f55c5a43a8d
Olivier CANO <oliviercano.mail@gmail.com>
2015-12-03 15:12:50 +0100

    dirscan fonctionne

--- dirscan.go
+++ dirscan.go
@@ -1,59 +1,70 @@
 package main
 
 import (
-    "fmt"
-    "io/ioutil"
-    "os"
-    "strings"
+	"fmt"
+	"io/ioutil"
+	"os"
 )
 
 type fic struct {
-  nom string 
-  lon int64
+	nom string
+	lon int64
 }
- 
+
 type fol struct {
-  subFol []fol
-  files  []fic
+	subFol []fol
+	files  []fic
+	nom    string
 }
 
-func scanDir(folder string) (fol) {
-  var listeRep fol
-      files, _ := ioutil.ReadDir(folder)
-      for _, f := range files {
-        var curNam string=f.Name()
+func scanDir(folder string) (fol, error) {
+	var listeRep fol
+	listeRep.nom = folder
+	files, err := ioutil.ReadDir(folder)
+	if err != nil {
+		return listeRep, err
+	}
+	for _, f := range files {
+		var curNam string = f.Name()
 
-        fi, _ := os.Open(f.Name())
-        stat, _ := fi.Stat()
-        var curSiz int64=stat.Size()
+		fi, err := os.Open(folder + f.Name())
+		defer fi.Close()
+		if err != nil {
+			return listeRep, err
+		}
+		stat, err := fi.Stat()
+		if err != nil {
 
-        if stat.IsDir(){
-          s := []string{folder, curNam}
-          var newPath string = strings.Join(s, "/")
+			return listeRep, err
+		}
+		var curSiz int64 = stat.Size()
 
-          //fil := fic{nom: curNam, lon: curSiz}
-          //fol := rep{subRep: scanDir(newPath), files: fil}
-          //listeRep.subRep=append(listeRep.subRep, fol)
+		if stat.IsDir() {
+			var newPath string = folder + curNam + "/"
 
-          fil := fic{nom: newPath, lon: curSiz}
-          listeRep.files=append(listeRep.files, fil)
+			fol, err := scanDir(newPath)
+			if err != nil {
+				return listeRep, err
+			}
+			listeRep.subFol = append(listeRep.subFol, fol)
 
-        } else {
-          fil := fic{nom: curNam, lon: curSiz}
-          listeRep.files=append(listeRep.files, fil)
-        }
-      }
-  return listeRep
+		} else {
+			fil := fic{nom: curNam, lon: curSiz}
+			listeRep.files = append(listeRep.files, fil)
+		}
+	}
+	return listeRep, err
 }
 
-func checkErr(err error){
-  if err != nil{
-    fmt.Fprintf(os.Stderr, "Fatal error : %s", err.Error())
-    os.Exit(1)
-  }
+func check(err error) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fatal error : %s", err.Error())
+		os.Exit(1)
+	}
 }
 
 func main() {
-  listRep := scanDir("./")
-  fmt.Println(listRep)
+	listRep, err := scanDir("./files/")
+	check(err)
+	fmt.Println(listRep)
 }
