fb27616a29b7b1316fc8c5a95f07ec9ccd69ede5
Adrien CHAPELET <adrien3d@gmail.com>
2015-12-03 17:44:37 +0100

    Implémentation md5

--- dirscan.go
+++ dirscan.go
@@ -4,6 +4,8 @@ import (
     "fmt"
     "io/ioutil"
     "os"
+    "io"
+    "crypto/md5"
     "time"
     "strconv"
 )
@@ -12,23 +14,39 @@ type fic struct {
   nom string 
   lon int64
   tim time.Time
+  md5hash []byte
 }
  
 type fol struct {
   subFol []fol
   files  []fic
   nom string
-  lon int64
   tim time.Time
 }
 
+func calculerMd5(filePath string) ([]byte, error) {
+  var result []byte
+  file, err := os.Open(filePath)
+  if err != nil {
+    return result, err
+  }
+  defer file.Close()
+ 
+  hash := md5.New()
+  if _, err := io.Copy(hash, file); err != nil {
+    return result, err
+  }
+ 
+  return hash.Sum(result), nil
+}
+
 func (myFile fic) fileToString() string {
-  var toPrint string = "\n\t"+myFile.nom +"\t"+ strconv.FormatInt(myFile.lon, 10) +"\t\t"+ myFile.tim.Format("02/01/2006 15:04:05")
+  var toPrint string = "\n\t"+myFile.nom +"\t"+ strconv.FormatInt(myFile.lon, 10) +"\t\t"+ myFile.tim.Format("02/01/2006 15:04:05")+"\t\t"+ string(myFile.md5hash[:])
   return toPrint;
 }
 
 func (myFolder fol) folderToString() string {
-  var toPrint string = "\n"+myFolder.nom +"\t\t"+ strconv.FormatInt(myFolder.lon, 10) +"\t\t"+ myFolder.tim.Format("02/01/2006 15:04:05")
+  var toPrint string = "\n"+myFolder.nom +"\t\t"+ myFolder.tim.Format("02/01/2006 15:04:05")
  for _,fi := range myFolder.files {
       toPrint += fi.fileToString()
     }
@@ -62,20 +80,20 @@ func scanDir(folder string) (fol, error) {
 		var curSiz int64 = stat.Size()
     lastModifTime := stat.ModTime()
 
-    listeRep.lon = curSiz
     listeRep.tim = lastModifTime
 
-		if stat.IsDir() {
-			var newPath string = folder + curNam + "/"
+    var newPath string = folder + curNam + "/"
 
+		if stat.IsDir() {
 			fol, err := scanDir(newPath)
 			if err != nil {
 				return listeRep, err
 			}
 			listeRep.subFol = append(listeRep.subFol, fol)
-
 		} else {
-			fil := fic{nom: curNam, lon: curSiz, tim: lastModifTime}
+      hsh, _ := calculerMd5(curNam); 
+      fmt.Printf("\nmd5 checksum is: %x", hsh)
+			fil := fic{nom: curNam, lon: curSiz, tim: lastModifTime, md5hash:hsh}
 			listeRep.files = append(listeRep.files, fil)
 		}
 	}
@@ -99,4 +117,10 @@ func main() {
   listRep, err := scanDir(dir)
   check(err)
 	fmt.Println(listRep.folderToString())
+
+  /*if b, err := calculerMd5("dirscan.go"); err != nil {
+    fmt.Printf("Err: %v", err)
+  } else {
+    fmt.Printf("main.go md5 checksum is: %x", b)
+  }*/
 }
